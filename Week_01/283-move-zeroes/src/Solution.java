dd3c89888e148240d1007b23d4f5c67b81f1e49c
leonluo <luo_li_ang@163.com>
2020-06-23 01:09:05 +0800

    add 2nd solution for move zeroes

--- Week_01/283-move-zeroes/src/Solution.java
+++ Week_01/283-move-zeroes/src/Solution.java
@@ -22,11 +22,31 @@ public class Solution {
         }
     }
 
+    void moveZeroes2(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        int lastNonZeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroIndex++] = nums[i];
+            }
+        }
+
+        for (int i = lastNonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] c = new int[] {0, 0, 0 ,1 ,3, 6, 0, 1, 0};
-        new Solution().moveZeroes(c);
+        int[] c = new int[] {0, 0, 0 , 1, 3, 6, 0, 1, 0};
+        Solution solution = new Solution();
+        solution.moveZeroes(c);
         System.out.println(Arrays.toString(c));
 
+        int[] c1 = new int[] {0, 0 ,1, 0, 6, 0, 0, 0, 3};
+        new Solution().moveZeroes2(c1);
+        System.out.println(Arrays.toString(c1));
     }
 
 }
