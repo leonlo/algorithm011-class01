3efd697e41af05754cfeae9aa636215d4f4c7fc8
leonluo <coder.leonluo@gmail.com>
2020-08-02 23:47:34 +0800

    problem 120 triangle

--- Week_06/120-triangle/src/Solution.java
+++ Week_06/120-triangle/src/Solution.java
@@ -42,6 +42,11 @@
 import java.util.List;
 
 class Solution {
+    /**
+     * DP Solution
+     * @param triangle
+     * @return
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
         int[] dp = new int[triangle.size() + 1];
         for (int i = triangle.size() - 1; i >= 0; i--) {
@@ -50,5 +55,51 @@ class Solution {
             }
         }
         return dp[0];
+    }
+
+
+    /**
+     * recursion solution
+     * @param triangle
+     * @return
+     */
+    private int row;
+
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        row = triangle.size();
+        return recursion(0, 0, triangle);
+    }
+
+    private int recursion(int level, int c, List<List<Integer>> triangle) {
+        if (level == row - 1) {
+            return triangle.get(level).get(c);
+        }
+        int left = recursion(level + 1, c, triangle);
+        int right = recursion(level + 1, c + 1, triangle);
+        return Math.min(left, right) + triangle.get(level).get(c);
+    }
+
+
+    /**
+     * memorized recursion solution
+     */
+    private Integer[][] memo;
+
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        row = triangle.size();
+        memo = new Integer[row][row];
+        return memorizedRecursion(0, 0, triangle);
+    }
+
+    private int memorizedRecursion(int level, int c, List<List<Integer>> triangle) {
+        if (memo[level][c] != null) {
+            return memo[level][c];
+        }
+        if (level == row - 1) {
+            return memo[level][c] = triangle.get(level).get(c);
+        }
+        int left = memorizedRecursion(level + 1, c, triangle);
+        int right = memorizedRecursion(level + 1, c + 1, triangle);
+        return memo[level][c] = Math.min(left, right) + triangle.get(level).get(c);
     }
 }
